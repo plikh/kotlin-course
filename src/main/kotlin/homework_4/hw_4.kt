bab627e901b4a1c4325b0288a93e8051e35bbfc2
pavel likholap <pavel.likholap@gmail.com>
2024-09-15 14:41:55 +0200

    hw4

---
+++ src/main/kotlin/homework_4/hw_4.kt
@@ -0,0 +1,46 @@
+package com.pavel.likholap.homework_4
+
+/*
+
+42: Byte
+98765432123456789L: Long
+23.45f: Float
+0.123456789: Float
+"Kotlin & Java": String
+false: Boolean
+'c': Char
+500: Short
+4294967296L: Long
+18.0f: Float
+-0.001: Float
+"OpenAI": String
+“true”: String
+'9': Char
+2048: Short
+10000000000L: Long
+5.75f: Float
+1.414: Float
+"Artificial Intelligence": String
+false: Boolean
+'@': Char
+1024: Short
+1234567890123L: Long
+10.01f: Float
+-273.15: Short
+"SpaceX": String
+true: Boolean
+“🤯”: String
+‘65535’: wrong value (for str need to be "65535")
+72057594037927935L: Long
+2.71828f: Float
+101.0101: Float
+"Quantum Computing": String
+false: Boolean
+'x': Char
+314: Short
+123456789123456789L: Long
+6.626f: Float
+0.007: Float
+"Android Studio": String
+
+*/
